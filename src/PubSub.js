67213777e5e8fdc493b6026678617d901dd4923d
Anthony Lannutti <lannuttia@gmail.com>
2019-05-19 22:37:04 +0000

    Added documentation to PubSub.js

--- src/PubSub.js
+++ src/PubSub.js
@@ -1,6 +1,18 @@
+/**
+ * @class PubSub
+ * @classdesc This class implements a publish/subscribe broker that can be used to
+ *            passively receive updates. This would typically be used with a
+ *            technology such as WebSockets or an HTML5 postMessage.
+ */
 export default class PubSub {
   #subscriptions = new Map();
 
+  /**
+   * @method
+   * @name PubSub#publish
+   * @param {*} topic - The topic for the payload to be published on.
+   * @param {*} payload - The payload that will be published.
+   */
   publish(topic, payload) {
     if (this.#subscriptions.has(topic)) {
       const callbacks = this.#subscriptions.get(topic);
@@ -10,6 +22,20 @@ export default class PubSub {
     }
   }
 
+  /**
+   * Callback for handling the published payload.
+   *
+   * @callback handlePayload
+   * @param {*} payload - The payload that was published.
+   */
+
+  /**
+   * @method
+   * @name PubSub#subscribe
+   * @param {*} topic - The topic of interest.
+   * @param {*} key - The value that is used to uniquely identify the callback.
+   * @param {handlePayload} callback - The callback that will be ran when a payload is published.
+   */
   subscribe(topic, key, callback) {
     if (this.#subscriptions.has(topic)) {
       const map = this.#subscriptions.get(topic);
@@ -19,6 +45,13 @@ export default class PubSub {
     }
   }
 
+  /**
+   * @method
+   * @name PubSub#unsubscribe
+   * @param {*} topic - The topic that should be unsubscribed from.
+   * @param {*} key - The key that was used when subscribing.
+   * @returns {boolean} - True if the unsubscription was successful, false otherwise.
+   */
   unsubscribe(topic, key) {
     if (this.#subscriptions.has(topic)) {
       const map = this.#subscriptions.get(topic);
